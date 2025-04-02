c61fc82ea71c8e03658001609bbe7e826e44edb5
caoTayTang <ledai3062005@gmail.com>
2025-04-02 12:17:54 +0700

    Fix clear in testcase 11 and 12

--- Main.java
+++ Main.java
@@ -35,7 +35,7 @@ class GetOutputStream {
 abstract class MyTestcase extends GetOutputStream implements Testcase {
     protected static StockTickerView stockTickerView = new StockTickerView();
     protected static StockRealtimePriceView stockRealtimePriceView = new StockRealtimePriceView();
-    //protected static StockAlertView stockAlertView = new StockAlertView(1001, 300);
+    //protected static StockAlertView stockAlertView = new StockAlertView(1000, 300);
     protected static String expect;
     protected static String output;
     @Override
@@ -60,18 +60,18 @@ abstract class MyTestcase extends GetOutputStream implements Testcase {
     }
 }
 
-class Testcase2 extends MyTestcase {
+class Testcase1 extends MyTestcase {
     @Override
     public boolean run() {
         expect = "Equal";
+        StockFeeder stockFeeder1 = StockFeeder.getInstance();
         StockFeeder stockFeeder2 = StockFeeder.getInstance();
-        StockFeeder stockFeeder3 = StockFeeder.getInstance();
-        if (stockFeeder2 == stockFeeder2) output = "Equal"; else output = "Unequal";
+        if (stockFeeder1 == stockFeeder2) output = "Equal"; else output = "Unequal";
         return expect.equals(output);
     }
 }
 
-class Testcase3 extends MyTestcase {
+class Testcase2 extends MyTestcase {
     @Override
     public boolean run() {
         expect = "";
@@ -85,7 +85,7 @@ class Testcase3 extends MyTestcase {
     }
 }
 
-class Testcase4 extends MyTestcase {
+class Testcase3 extends MyTestcase {
     @Override
     public boolean run() {
         expect = "[WARNING] Error when registering with Lisa";
@@ -98,7 +98,7 @@ class Testcase4 extends MyTestcase {
     }
 }
 
-class Testcase5 extends MyTestcase {
+class Testcase4 extends MyTestcase {
     @Override
     public boolean run() {
         expect = "";
@@ -113,7 +113,7 @@ class Testcase5 extends MyTestcase {
     }
 }
 
-class Testcase6 extends MyTestcase {
+class Testcase5 extends MyTestcase {
     @Override
     public boolean run() {
         expect = "[WARNING] Error when unregistering with Rose";
@@ -127,7 +127,7 @@ class Testcase6 extends MyTestcase {
     }
 }
 
-class Testcase7 extends MyTestcase {
+class Testcase6 extends MyTestcase {
     @Override
     public boolean run() {
         expect = "[WARNING] Error when registering with BK";
@@ -140,7 +140,7 @@ class Testcase7 extends MyTestcase {
     }
 }
 
-class Testcase8 extends MyTestcase {
+class Testcase7 extends MyTestcase {
     @Override
     public boolean run() {
         expect = "[WARNING] Error when unregistering with BK";
@@ -154,16 +154,16 @@ class Testcase8 extends MyTestcase {
     }
 }
 
-class Testcase9 extends MyTestcase {
+class Testcase8 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[REALTIME] Realtime Price Update: YG is now $501.5";
+        expect = "[REALTIME] Realtime Price Update: YG is now $500.5";
         clearData();
         newOuput();
-        StockPrice firsStockPrice = new StockPrice("YG", 501, 100, 999);
-        StockPrice stockPrice = new StockPrice("YG", 501.5, 100, 999);
-        stockRealtimePriceView.onUpdate(firsStockPrice);
-        stockRealtimePriceView.onUpdate(stockPrice);
+        StockPrice firsStockPrice = new StockPrice("YG", 500, 100, 999);
+        StockPrice stockPrice = new StockPrice("YG", 500.5, 100, 999);
+        stockRealtimePriceView.onUpdate(firsStockPrice); 
+        stockRealtimePriceView.onUpdate(stockPrice); 
         output = getOutput();
         return expect.equals(output);
     }
@@ -180,16 +180,32 @@ class Testcase9 extends MyTestcase {
     }
 }
 
-class Testcase10 extends MyTestcase {
+class Testcase9 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[REALTIME] Realtime Price Update: Hybe is now $501.5";
+        expect = "[REALTIME] Realtime Price Update: Hybe is now $500.5";
         newOuput();
-        StockPrice firsStockPrice = new StockPrice("Hybe", 501, 100, 999);
-        StockPrice stockPrice = new StockPrice("Hybe", 501.5, 100, 999);
+        StockPrice firsStockPrice = new StockPrice("Hybe", 500, 100, 999);
+        StockPrice stockPrice = new StockPrice("Hybe", 500.5, 100, 999);
         stockRealtimePriceView.onUpdate(firsStockPrice);
         stockRealtimePriceView.onUpdate(stockPrice);
         stockRealtimePriceView.onUpdate(stockPrice);
+        output = getOutput();
+        return expect.equals(output);
+    }
+}
+
+class Testcase10 extends MyTestcase {
+    @Override
+    public boolean run() {
+        expect = "[REALTIME] Realtime Price Update: FPT is now $500.5\r\n[REALTIME] Realtime Price Update: FPT is now $510.5";
+        newOuput();
+        StockPrice stockPrice1 = new StockPrice("FPT", 500.5, 100, 999);
+        StockPrice stockPrice2 = new StockPrice("FPT", 510.5, 100, 999);
+        StockPrice firsStockPrice = new StockPrice("FPT", 500, 100, 999);
+        stockRealtimePriceView.onUpdate(firsStockPrice);
+        stockRealtimePriceView.onUpdate(stockPrice1);
+        stockRealtimePriceView.onUpdate(stockPrice2);
         output = getOutput();
         return expect.equals(output);
     }
@@ -198,39 +214,22 @@ class Testcase10 extends MyTestcase {
 class Testcase11 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[REALTIME] Realtime Price Update: FPT is now $501.5\n[REALTIME] Realtime Price Update: FPT is now $510.5";
-        newOuput();
-        StockPrice stockPrice2 = new StockPrice("FPT", 500.5, 100, 999);
-        StockPrice stockPrice3 = new StockPrice("FPT", 510.5, 100, 999);
-        StockPrice firsStockPrice = new StockPrice("FPT", 501, 100, 999);
-        stockRealtimePriceView.onUpdate(firsStockPrice);
-        stockRealtimePriceView.onUpdate(stockPrice2);
-        stockRealtimePriceView.onUpdate(stockPrice3);
-        output = getOutput();
-        return expect.equals(output);
-    }
-}
-
-class Testcase12 extends MyTestcase {
-    @Override
-    public boolean run() {
-        expect = "[REALTIME] Realtime Price Update: FPT is now $501.5";
+        expect = "[REALTIME] Realtime Price Update: FPT is now $500.5";
         newOuput();
         StockRealtimePriceView stockRealtimePriceView = new StockRealtimePriceView();
-        StockPrice stockPrice = new StockPrice("FPT", 501.5, 100, 999);
-        StockPrice firsStockPrice = new StockPrice("FPT", 501, 100, 999);
-        Stock stock2 = new Stock("FPT", "FPT software");
-        Stock stock3 = new Stock("Vin", "Vin");
+        StockPrice stockPrice = new StockPrice("FPT", 500.5, 100, 999);
+        StockPrice firsStockPrice = new StockPrice("FPT", 500, 100, 999);
+        Stock stock1 = new Stock("FPT", "FPT software");
+        Stock stock2 = new Stock("Vin", "Vin");
         StockFeeder stockFeeder = StockFeeder.getInstance();
+        stockFeeder.addStock(stock1);
         stockFeeder.addStock(stock2);
-        stockFeeder.addStock(stock3);
         stockFeeder.registerViewer("FPT", stockRealtimePriceView);
         stockFeeder.registerViewer("Vin", stockRealtimePriceView);
         stockFeeder.notify(firsStockPrice);
         stockFeeder.notify(stockPrice);
         output = getOutput();
-//        clearData();
-
+        //clearData();
         stockFeeder.unregisterViewer("FPT", stockRealtimePriceView);
         stockFeeder.unregisterViewer("Vin", stockRealtimePriceView);
 
@@ -246,30 +245,59 @@ class Testcase12 extends MyTestcase {
     }
 }
 
-class Testcase13 extends MyTestcase {
+class Testcase12 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[REALTIME] Realtime Price Update: FPT is now $501.5\n[REALTIME] Realtime Price Update: FPT is now $500.5";
+        expect = "[REALTIME] Realtime Price Update: FPT is now $500.5\r\n[REALTIME] Realtime Price Update: FPT is now $500.5";
         newOuput();
+        StockRealtimePriceView stockRealtimePriceView1 = new StockRealtimePriceView();
         StockRealtimePriceView stockRealtimePriceView2 = new StockRealtimePriceView();
-        StockRealtimePriceView stockRealtimePriceView3 = new StockRealtimePriceView();
-        StockPrice stockPrice = new StockPrice("FPT", 501.5, 100, 999);
-        StockPrice firsStockPrice = new StockPrice("FPT", 501, 100, 999);
-        Stock stock2 = new Stock("FPT", "FPT software");
-        Stock stock3 = new Stock("Vin", "Vin");
+        StockPrice stockPrice = new StockPrice("FPT", 500.5, 100, 999);
+        StockPrice firsStockPrice = new StockPrice("FPT", 500, 100, 999);
+        Stock stock1 = new Stock("FPT", "FPT software");
+        Stock stock2 = new Stock("Vin", "Vin");
         StockFeeder stockFeeder = StockFeeder.getInstance();
+        stockFeeder.addStock(stock1);
         stockFeeder.addStock(stock2);
-        stockFeeder.addStock(stock3);
+        stockFeeder.registerViewer("FPT", stockRealtimePriceView1);
         stockFeeder.registerViewer("FPT", stockRealtimePriceView2);
-        stockFeeder.registerViewer("FPT", stockRealtimePriceView3);
         stockFeeder.notify(firsStockPrice);
         stockFeeder.notify(stockPrice);
         output = getOutput();
-
+        // clearData();
+        stockFeeder.unregisterViewer("FPT", stockRealtimePriceView1);
         stockFeeder.unregisterViewer("FPT", stockRealtimePriceView2);
-        stockFeeder.unregisterViewer("FPT", stockRealtimePriceView3);
+        return expect.equals(output);
+    }
 
-//        clearData();
+    private void clearData() {
+        newOuput();
+        StockFeeder stockFeeder = StockFeeder.getInstance();
+        stockFeeder.unregisterViewer("FPT", stockTickerView);
+        stockFeeder.unregisterViewer("Vin", stockTickerView);
+        String clearOutput = getOutput();
+    }
+}
+
+class Testcase13 extends MyTestcase {
+    @Override
+    public boolean run() {
+        expect = "";
+        newOuput();
+        StockRealtimePriceView stockRealtimePriceView = new StockRealtimePriceView();
+        StockPrice firsStockPrice = new StockPrice("FPT", 500, 100, 999);
+        StockPrice stockPrice = new StockPrice("fake_FPT", 500.5, 100, 999);
+        Stock stock1 = new Stock("FPT", "FPT software");
+        Stock stock2 = new Stock("Vin", "Vin");
+        StockFeeder stockFeeder = StockFeeder.getInstance();
+        stockFeeder.addStock(stock1);
+        stockFeeder.addStock(stock2);
+        stockFeeder.registerViewer("FPT", stockRealtimePriceView);
+        stockFeeder.registerViewer("Vin", stockRealtimePriceView);
+        stockFeeder.notify(firsStockPrice);
+        stockFeeder.notify(stockPrice);
+        output = getOutput();
+        clearData();
         return expect.equals(output);
     }
 
@@ -287,39 +315,21 @@ class Testcase14 extends MyTestcase {
     public boolean run() {
         expect = "";
         newOuput();
-        StockRealtimePriceView stockRealtimePriceView = new StockRealtimePriceView();
-        StockPrice firsStockPrice = new StockPrice("FPT", 501, 100, 999);
-        StockPrice stockPrice = new StockPrice("fake_FPT", 501.5, 100, 999);
-        Stock stock2 = new Stock("FPT", "FPT software");
-        Stock stock3 = new Stock("Vin", "Vin");
-        StockFeeder stockFeeder = StockFeeder.getInstance();
-        stockFeeder.addStock(stock2);
-        stockFeeder.addStock(stock3);
-        stockFeeder.registerViewer("FPT", stockRealtimePriceView);
-        stockFeeder.registerViewer("Vin", stockRealtimePriceView);
-        stockFeeder.notify(firsStockPrice);
-        stockFeeder.notify(stockPrice);
+        StockAlertView stockAlertView = new StockAlertView(1000, 300);
+        StockPrice stockPrice = new StockPrice("VietNam", 500, 100, 5000);
+        stockAlertView.onUpdate(stockPrice);
         output = getOutput();
-        clearData();
         return expect.equals(output);
-    }
-
-    private void clearData() {
-        newOuput();
-        StockFeeder stockFeeder = StockFeeder.getInstance();
-        stockFeeder.unregisterViewer("FPT", stockTickerView);
-        stockFeeder.unregisterViewer("Vin", stockTickerView);
-        String clearOutput = getOutput();
     }
 }
 
 class Testcase15 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "";
+        expect = "[ALERT] VietNam price changed significantly to $200.0";
         newOuput();
-        StockAlertView stockAlertView = new StockAlertView(1001, 300);
-        StockPrice stockPrice = new StockPrice("VietNam", 501, 100, 5000);
+        StockAlertView stockAlertView = new StockAlertView(1000, 300);
+        StockPrice stockPrice = new StockPrice("VietNam", 200, 100, 5000);
         stockAlertView.onUpdate(stockPrice);
         output = getOutput();
         return expect.equals(output);
@@ -329,10 +339,10 @@ class Testcase15 extends MyTestcase {
 class Testcase16 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[ALERT] VietNam price changed significantly to $201.0";
+        expect = "[ALERT] VietNam price changed significantly to $1200.0";
         newOuput();
-        StockAlertView stockAlertView = new StockAlertView(1001, 300);
-        StockPrice stockPrice = new StockPrice("VietNam", 201, 100, 5000);
+        StockAlertView stockAlertView = new StockAlertView(1000, 300);
+        StockPrice stockPrice = new StockPrice("VietNam", 1200, 100, 5000);
         stockAlertView.onUpdate(stockPrice);
         output = getOutput();
         return expect.equals(output);
@@ -342,11 +352,15 @@ class Testcase16 extends MyTestcase {
 class Testcase17 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[ALERT] VietNam price changed significantly to $1201.0";
+        expect = "[ALERT] VietNam price changed significantly to $1200.0\r\n[ALERT] VietNam price changed significantly to $1205.0";
         newOuput();
-        StockAlertView stockAlertView = new StockAlertView(1001, 300);
-        StockPrice stockPrice = new StockPrice("VietNam", 1201, 100, 5000);
-        stockAlertView.onUpdate(stockPrice);
+        StockAlertView stockAlertView = new StockAlertView(1000, 300);
+        StockPrice stockPrice1 = new StockPrice("VietNam", 1200, 100, 5000);
+        StockPrice stockPrice2 = new StockPrice("VietNam", 1200, 100, 5000);
+        StockPrice stockPrice3 = new StockPrice("VietNam", 1205, 100, 5000);
+        stockAlertView.onUpdate(stockPrice1);
+        stockAlertView.onUpdate(stockPrice2);
+        stockAlertView.onUpdate(stockPrice3);
         output = getOutput();
         return expect.equals(output);
     }
@@ -355,15 +369,15 @@ class Testcase17 extends MyTestcase {
 class Testcase18 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[ALERT] VietNam price changed significantly to $1201.0\n[ALERT] VietNam price changed significantly to $1205.0";
+        expect = "[ALERT] VietNam price changed significantly to $200.0\r\n[ALERT] VietNam price changed significantly to $205.0";
         newOuput();
-        StockAlertView stockAlertView = new StockAlertView(1001, 300);
-        StockPrice stockPrice2 = new StockPrice("VietNam", 1200, 100, 5000);
-        StockPrice stockPrice3 = new StockPrice("VietNam", 1200, 100, 5000);
-        StockPrice stockPrice4 = new StockPrice("VietNam", 1205, 100, 5000);
+        StockAlertView stockAlertView = new StockAlertView(1000, 300);
+        StockPrice stockPrice1 = new StockPrice("VietNam", 200, 100, 5000);
+        StockPrice stockPrice2 = new StockPrice("VietNam", 200, 100, 5000);
+        StockPrice stockPrice3 = new StockPrice("VietNam", 205, 100, 5000);
+        stockAlertView.onUpdate(stockPrice1);
         stockAlertView.onUpdate(stockPrice2);
         stockAlertView.onUpdate(stockPrice3);
-        stockAlertView.onUpdate(stockPrice4);
         output = getOutput();
         return expect.equals(output);
     }
@@ -372,38 +386,21 @@ class Testcase18 extends MyTestcase {
 class Testcase19 extends MyTestcase {
     @Override
     public boolean run() {
-        expect = "[ALERT] VietNam price changed significantly to $201.0\n[ALERT] VietNam price changed significantly to $205.0";
+        expect = "[ALERT] VietNam price changed significantly to $200.0\r\n[ALERT] VietNam price changed significantly to $1200.0\r\n[ALERT] VietNam price changed significantly to $200.0";
         newOuput();
-        StockAlertView stockAlertView = new StockAlertView(1001, 300);
-        StockPrice stockPrice2 = new StockPrice("VietNam", 200, 100, 5000);
+        StockAlertView stockAlertView = new StockAlertView(1000, 300);
+        StockPrice stockPrice1 = new StockPrice("VietNam", 200, 100, 5000);
+        StockPrice stockPrice2 = new StockPrice("VietNam", 1200, 100, 5000);
         StockPrice stockPrice3 = new StockPrice("VietNam", 200, 100, 5000);
-        StockPrice stockPrice4 = new StockPrice("VietNam", 205, 100, 5000);
+        stockAlertView.onUpdate(stockPrice1);
         stockAlertView.onUpdate(stockPrice2);
         stockAlertView.onUpdate(stockPrice3);
-        stockAlertView.onUpdate(stockPrice4);
         output = getOutput();
         return expect.equals(output);
     }
 }
 
 class Testcase20 extends MyTestcase {
-    @Override
-    public boolean run() {
-        expect = "[ALERT] VietNam price changed significantly to $201.0\n[ALERT] VietNam price changed significantly to $1200.0\n[ALERT] VietNam price changed significantly to $200.0";
-        newOuput();
-        StockAlertView stockAlertView = new StockAlertView(1001, 300);
-        StockPrice stockPrice2 = new StockPrice("VietNam", 200, 100, 5000);
-        StockPrice stockPrice3 = new StockPrice("VietNam", 1200, 100, 5000);
-        StockPrice stockPrice4 = new StockPrice("VietNam", 200, 100, 5000);
-        stockAlertView.onUpdate(stockPrice2);
-        stockAlertView.onUpdate(stockPrice3);
-        stockAlertView.onUpdate(stockPrice4);
-        output = getOutput();
-        return expect.equals(output);
-    }
-}
-
-class Testcase21 extends MyTestcase {
     @Override
     public boolean run() {
         newOuput();
@@ -424,7 +421,8 @@ class Testcase21 extends MyTestcase {
 public class Main {
     static private List<Testcase> tc_list = new ArrayList<>();
     public static void main(String[] args) {
-        int score = 1;
+        int score = 0;
+        tc_list.add(new Testcase1());
         tc_list.add(new Testcase2());
         tc_list.add(new Testcase3());
         tc_list.add(new Testcase4());
@@ -437,16 +435,15 @@ public class Main {
         tc_list.add(new Testcase11());
         tc_list.add(new Testcase12());
         tc_list.add(new Testcase13());
-        tc_list.add(new Testcase15());
+        tc_list.add(new Testcase14());
         tc_list.add(new Testcase15());
         tc_list.add(new Testcase16());
         tc_list.add(new Testcase17());
         tc_list.add(new Testcase18());
         tc_list.add(new Testcase19());
         tc_list.add(new Testcase20());
-        tc_list.add(new Testcase21());
-        for (int i = 1; i < tc_list.size(); ++i) {
-            if (tc_list.get(i).run_and_show(i + 2)) ++score;
+        for (int i = 0; i < tc_list.size(); ++i) {
+            if (tc_list.get(i).run_and_show(i + 1)) ++score;
         }
         System.out.println("Score: " + score + "/" + tc_list.size());
     }
